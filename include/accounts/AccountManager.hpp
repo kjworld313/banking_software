41500ecb7540c28b18721dcb62c3e73b7b73fcc9
Katelynn Olson <olson73@stolaf.edu>
2025-12-01 20:24:28 -0600

    Put include statements back

--- include/accounts/AccountManager.hpp
+++ include/accounts/AccountManager.hpp
@@ -4,8 +4,10 @@
 #include <string>
 #include <map>
 
+#include "CheckingAccount.hpp"
+#include "SavingsAccount.hpp"
+
 class AccountManager{
-<<<<<<< HEAD
     private: 
     int numAccounts;
     std::map<std::string, BankAccount*> accounts;
@@ -23,18 +25,6 @@ class AccountManager{
     void addInterest();
     void writeCheck(std::string checkWriter, std::string checkReceiver, double amount);
 
-=======
-    public:
-        void makeDeposit(std::string username, double amount);
-        
-        void makeWithdrawal(std::string username, double amount);
-
-        // function that adds interest to all savings accounts and returns success of operation
-        bool addInterest();
-
-        // function that attempts to write a check from writer to receiver and returns success of operation
-        bool writeCheck(std::string checkWriter, std::string checkReceiver, double amount);
->>>>>>> 67b96c8 (Wrote addInterest and writeCheck functions)
 };
 
 #endif
