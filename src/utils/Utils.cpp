61ad6513bd054e13e3464a9fbf8e0559ef4669e2
Omar Segura <segura4@rns203-5.cs.stolaf.edu>
2025-12-02 19:36:28 -0600

    finished getString for cpp

--- src/utils/Utils.cpp
+++ src/utils/Utils.cpp
@@ -1,24 +1,23 @@
 #include "Utils.hpp"
 #include <iostream>
 
+std::string getString(const std::string &prompt){
+    std::cout << prompt;
+    std::string input;
+    std::cin >> input;
+    return input;
+}
+
 BankAccount* make_account(){
     BankAccount *acct = nullptr;
 
-    std::cout << "Enter the first name:"; 
-    std::string first;
-    std::cin>> first;
+    std::string first = getString("Enter the first name ");
+    std::string last = getString("Enter the last name ");
 
-    std::cout << "Enter the last name:";
-    std::string last;
-    std::cin>> last;
+    std::string choice = getString("Would you like tomake a savings (1) or checking (2) account: ");
 
-    std::cout << "Would you like to make a savings (1) or checking (2) account:" << std::endl;
-    std::string choice;
-    std::cin>> choice;
     if(choice == "1"){
-        std::cout << "Enter the interest rate:";
-        std::string interest_rate_string;
-        std::cin>> interest_rate_string;
+        std::string interest_rate_string = getString("Enter the interest rate: ");
 
         char *end;
         double interest_rate = strtod(interest_rate_string.c_str(), &end);
