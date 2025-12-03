59ebc8ef1bebf4a90bc7288a0bcd260b8583a0df
Lucy Blaney <blaney1@stolaf.edu>
2025-12-02 18:52:34 -0600

    withdrawal tests 1 and 2 handeling throw exception, revised tests to match how addAccount will work

--- test/AccountManagerTests.cpp
+++ test/AccountManagerTests.cpp
@@ -10,7 +10,6 @@ BOOST_AUTO_TEST_SUITE(account_manager_suite, * boost::unit_test::timeout(10))
 */
 BOOST_AUTO_TEST_CASE(makeDepositTest1){
     AccountManager accs;
-    SavingsAccount acc1 = SavingAccount("Lucy", "Blaney", 100.0, 0.5);
     
     accs.addAccount("lblaney");
     accs.makeDeposit("lblaney", 100.0);
@@ -24,10 +23,33 @@ BOOST_AUTO_TEST_CASE(makeDepositTest1){
 */
 BOOST_AUTO_TEST_CASE(makeDepositTest2){
     AccountManager accs;
-    SavingsAccount acc1 = SavingAccount("Lucy", "Blaney", 100.0, 0.5);
 
     accs.addAccount("lblaney");
-    BOOST_CHECK_THROW(accs.makeDeposit("bblaney", 100.0), std::out_of_range)
+    BOOST_CHECK_THROW(accs.makeDeposit("bblaney", 100.0), std::out_of_range);
+}
+
+/*
+ * Lines Covered: 
+ * Branches Covered: 
+*/
+BOOST_AUTO_TEST_CASE(makeWithdrawalTest1){
+    AccountManager accs;
+
+    accs.addAccount("lblaney");
+    accs.makeWithdrawal("lblaney", 50.0);
+
+    BOOST_CHECK_EQUAL(acc1.getBalance(), 50.0);
+}
+
+/*
+ * Lines Covered: 
+ * Branches Covered: 
+*/
+BOOST_AUTO_TEST_CASE(makeWithdrawalTest2){
+    AccountManager accs;
+
+    accs.addAccount("lblaney");
+    BOOST_CHECK_THROW(accs.makeWithdrawal("bblaney", 100.0), std::out_of_range);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
