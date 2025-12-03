f6568d632292b996fc0483c91dd655cd9f6e3451
Lucy Blaney <blaney1@stolaf.edu>
2025-12-02 18:39:56 -0600

    test 1 and 2 for deposit function handling both branches

--- test/AccountManagerTests.cpp
+++ test/AccountManagerTests.cpp
@@ -4,5 +4,30 @@
 
 BOOST_AUTO_TEST_SUITE(account_manager_suite, * boost::unit_test::timeout(10))
 
+/*
+ * Lines Covered: 
+ * Branches Covered: 
+*/
+BOOST_AUTO_TEST_CASE(makeDepositTest1){
+    AccountManager accs;
+    SavingsAccount acc1 = SavingAccount("Lucy", "Blaney", 100.0, 0.5);
+    
+    accs.addAccount("lblaney");
+    accs.makeDeposit("lblaney", 100.0);
+
+    BOOST_CHECK_EQUAL(acc1.getBalance(), 200.0);
+}
+
+/*
+ * Lines Covered: 
+ * Branches Covered: 
+*/
+BOOST_AUTO_TEST_CASE(makeDepositTest2){
+    AccountManager accs;
+    SavingsAccount acc1 = SavingAccount("Lucy", "Blaney", 100.0, 0.5);
+
+    accs.addAccount("lblaney");
+    BOOST_CHECK_THROW(accs.makeDeposit("bblaney", 100.0), std::out_of_range)
+}
 
 BOOST_AUTO_TEST_SUITE_END()
