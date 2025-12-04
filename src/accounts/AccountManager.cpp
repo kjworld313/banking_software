e69dd959bf15ff6d5777fd1b07a405beed6a0204
Omar Segura <segura4@rns203-5.cs.stolaf.edu>
2025-12-03 19:16:12 -0600

    add account for cpp2

--- src/accounts/AccountManager.cpp
+++ src/accounts/AccountManager.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <stdexcept>
 #include "../utils/Utils.hpp"
-
+//merging
 //check if username already exists
 void AccountManager::addAccount(std::string username){
     if accounts.find(username) != accounts.end()){
