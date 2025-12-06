e6b977f7d9e08d9bbe14df0e1509eab7f8551f3b
Omar Segura <segura4@rns203-5.cs.stolaf.edu>
2025-12-05 19:39:50 -0600

    Checking account test1

--- test/SavingsAccountTests.cpp
+++ test/SavingsAccountTests.cpp
@@ -2,6 +2,7 @@
 
 #include <boost/test/unit_test.hpp>
 #include "SavingsAccount.hpp"
+
 BOOST_AUTO_TEST_SUITE(savings_account_suite, * boost::unit_test::timeout(10))
 
 
@@ -14,11 +15,10 @@ BOOST_AUTO_TEST_SUITE(savings_account_suite, * boost::unit_test::timeout(10))
 BOOST_AUTO_TEST_CASE(constructorTest1){
     SavingsAccount* acc = new SavingsAccount("Lucy","Blaney", 100.0, 0.5);
 
-    BOOST_CHECK_EQUAL(acc.getFirstName(), "Lucy");
-    BOOST_CHECK_EQUAL(acc.getLastName(),"Blaney");
-    BOOST_CHECK_EQUAL(acc.getBalance(), 100.0);
-    BOOST_CHECK_EQUAL(acc.getInterestRate(), 0.5);
-
+    BOOST_CHECK_EQUAL(acc->getFirstName(), "Lucy");
+    BOOST_CHECK_EQUAL(acc->getLastName(),"Blaney");
+    BOOST_CHECK_EQUAL(acc->getBalance(), 100.0);
+    BOOST_CHECK_EQUAL(acc->getInterestRate(), 0.5);
 }
 
 /*
@@ -26,12 +26,12 @@ BOOST_AUTO_TEST_CASE(constructorTest1){
  * Branches Covered: N/A
 */
 BOOST_AUTO_TEST_CASE(constructorTest2){
-    SavingsAccount acc = new SavingsAccount("Lucy", "Blaney", 0.3);
+    SavingsAccount* acc = new SavingsAccount("Lucy", "Blaney", 0.3);
 
-    BOOST_CHECK_EQUAL(acc.getFirstName(), "Lucy");
-    BOOST_CHECK_EQUAL(acc.getLastName(),"Blaney");
-    BOOST_CHECK_EQUAL(acc.getInterestRate(), 0.3);
-    BOOST_CHECK_EQUAL(acc.getBalance(), 0.0);
+    BOOST_CHECK_EQUAL(acc->getFirstName(), "Lucy");
+    BOOST_CHECK_EQUAL(acc->getLastName(),"Blaney");
+    BOOST_CHECK_EQUAL(acc->getInterestRate(), 0.3);
+    BOOST_CHECK_EQUAL(acc->getBalance(), 0.0);
 }
 
 /*
@@ -39,11 +39,11 @@ BOOST_AUTO_TEST_CASE(constructorTest2){
  * Branches Tested: N/A
 */
 BOOST_AUTO_TEST_CASE(accrueInterest_test){
-    SavingsAccount acc =  SavingsAccount("Lucy","Blaney", 100.0, 0.1);
+    SavingsAccount* acc = new SavingsAccount("Lucy","Blaney", 100.0, 0.1);
 
-    acc.accrueInterest();
+    acc->accrueInterest();
 
-    BOOST_CHECK_CLOSE(acc.getBalance(), 110.0, 0.001);
+    BOOST_CHECK_CLOSE(acc->getBalance(), 110.0, 0.01);
 }
 
 
