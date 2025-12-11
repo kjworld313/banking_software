974b3d8e16d8c9bf3781a61a16277ef160ccb85f
Katelynn Olson <olson73@stolaf.edu>
2025-12-10 19:43:18 -0600

    Constructor 2 test for checking account tests, test for checking account serialize, updated some naming convention stuff to make sure consistent

--- test/CheckingAccountTests.cpp
+++ test/CheckingAccountTests.cpp
@@ -7,15 +7,20 @@
 BOOST_AUTO_TEST_SUITE(checking_account_suite, * boost::unit_test::timeout(10))
 
 /**
- * Lines Covered: 9 (hpp)
+ * Lines Covered: 9, 10 (hpp)
  * Branches Covered: N/A
  */
-BOOST_AUTO_TEST_CASE(constructor_test) {
+BOOST_AUTO_TEST_CASE(constructor_test1) {
     CheckingAccount* acc = new CheckingAccount("omar", "segura", 50.0);
+    CheckingAccount* acc2 = new CheckingAccount("omar", "segura");
 
     BOOST_CHECK_EQUAL(acc->getFirstName(), "omar");
     BOOST_CHECK_EQUAL(acc->getLastName(), "segura");
     BOOST_CHECK_EQUAL(acc->getBalance(), 50.0);
+
+    BOOST_CHECK_EQUAL(acc2->getFirstName(), "omar");
+    BOOST_CHECK_EQUAL(acc2->getLastName(), "segura");
+    BOOST_CHECK_EQUAL(acc2->getBalance(), 0.0);
 
     delete acc; 
 
@@ -141,5 +146,22 @@ BOOST_AUTO_TEST_CASE(check_log_test){
     delete acc;
     delete acc2;
 }
+
+/**
+ * Lines Covered: 9, 10 (hpp); 4, 6, 11, 19, 20, 22, 23, 25(cpp)
+ * Branches Covered: 6T, 22T, 22F
+ */
+
+ BOOST_AUTO_TEST_CASE(serialize_test) {
+    CheckingAccount* acc = new CheckingAccount("omar", "segura", 10.00);
+    CheckingAccount* acc2 = new CheckingAccount("john", "Marston", 50.0);
+    acc->writeCheck(*acc2, 10.0);
+    std::string expected_string =  "omar1,checking," + acc->getFirstName() + "," + acc->getLastName() + "," 
+    + std::to_string(acc->getBalance()) + ",10.000000";
+
+    std::string result = acc->serialize("omar1");
+
+    BOOST_CHECK_EQUAL(result, expected_string);
+ }
 
 BOOST_AUTO_TEST_SUITE_END()
