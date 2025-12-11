ef954ceec11d01a250bc0732edbdefe0b831fb16
Katelynn Olson <olson73@stolaf.edu>
2025-12-10 19:24:25 -0600

    Added line coverage branch coverage comments to checking account tests

--- test/SavingsAccountTests.cpp
+++ test/SavingsAccountTests.cpp
@@ -8,11 +8,11 @@ BOOST_AUTO_TEST_SUITE(savings_account_suite, * boost::unit_test::timeout(10))
 
 /*
 
- * Lines covered: 3, 4
+ * Lines covered: 4, 26
  * branches covered: N/A
 
 */
-BOOST_AUTO_TEST_CASE(constructorTest1){
+BOOST_AUTO_TEST_CASE(constructor_test1){
     SavingsAccount* acc = new SavingsAccount("Lucy","Blaney", 100.0, 0.5);
 
     BOOST_CHECK_EQUAL(acc->getFirstName(), "Lucy");
@@ -22,10 +22,10 @@ BOOST_AUTO_TEST_CASE(constructorTest1){
 }
 
 /*
- * Lines Covered: 7, 8
+ * Lines Covered: 8, 26
  * Branches Covered: N/A
 */
-BOOST_AUTO_TEST_CASE(constructorTest2){
+BOOST_AUTO_TEST_CASE(constructor_test2){
     SavingsAccount* acc = new SavingsAccount("Lucy", "Blaney", 0.3);
 
     BOOST_CHECK_EQUAL(acc->getFirstName(), "Lucy");
@@ -35,10 +35,10 @@ BOOST_AUTO_TEST_CASE(constructorTest2){
 }
 
 /*
- * Lines Tested: 11, 12, 13
+ * Lines Tested: 4, 12, 13
  * Branches Tested: N/A
 */
-BOOST_AUTO_TEST_CASE(accrueInterest_test){
+BOOST_AUTO_TEST_CASE(accrue_interest_test){
     SavingsAccount* acc = new SavingsAccount("Lucy","Blaney", 100.0, 0.1);
 
     acc->accrueInterest();
